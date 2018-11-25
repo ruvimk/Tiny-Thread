a912bd690ff4198a861b3ac918e6d47e621c0f5d
Рувим Кондратьев <ruvim.k.1@gmail.com>
2018-11-25 09:43:16 -0800

    Update avr-test.c to reflect the changes we've made

--- avr-test.c
+++ avr-test.c
@@ -1,35 +1,35 @@
-#include <avr/interrupt.h>
 #include "thread.h"
 
-void test1 (void) {
+TT_MUTEX mtx_leds; 
+
+TT_THREAD thread1; 
+uint8_t thread1_stack [TT_MIN_STACK_SIZE + 32]; 
+void thread1_code (void) {
 	while (1) {
-		TT_CLI ();
-		PORTB = (PORTB & ~3) | BIT (1);
-		TT_STI ();
-		tt_sleep_ticks (5 << 8);
+		tt_mutex_lock (&mtx_leds); 
+		PORTB = (PORTB & ~3) | (1<<1);
+		tt_mutex_unlock (&mtx_leds); 
+		tt_sleep_ms (300);
 	}
 }
 
 int main (void) {
-	DDRE |= BIT (5) | BIT (6) | BIT (7);
 	DDRB = 0xFF;
 	tt_init ();
-	void * stack1 [TT_MIN_STACK_SIZE / sizeof (void *)];
-	TT_THREAD thread1 = {
-		tt_prepare_stack (stack1, sizeof (stack1), test1),
-		TT_PRIORITY_NORMAL,
-		0, // Immediately.
-		0
+	thread1 = {
+		tt_prepare_stack (thread1_stack, 
+						  sizeof (thread1_stack), 
+						  thread1_code), 
+		TT_PRIORITY_NORMAL, 
+		0, 0, 0 
 	};
 	tt_add_thread (&thread1);
 	sei ();
 	while (1) {
-		TT_CLI ();
-		PORTB &= ~(BIT (0) | BIT (1));
-		PORTE ^= BIT (6);
-		PORTE ^= BIT (6);
-		TT_STI ();
-		tt_sleep_ticks (5 << 8);
+		tt_mutex_lock (&mtx_leds); 
+		PORTB &= ~3;
+		tt_mutex_unlock (&mtx_leds); 
+		tt_sleep_ms (500); 
 	}
 	return 0;
 }
